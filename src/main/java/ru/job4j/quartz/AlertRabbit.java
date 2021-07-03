616accadec13e120a68290565a38b430b3e6fdd5
Azamat-Sult <asultangareev@gmail.com>
2021-07-03 18:13:32 +0500

    1. Quartz [#175122]

---
+++ src/main/java/ru/job4j/quartz/AlertRabbit.java
@@ -0,0 +1,56 @@
+package ru.job4j.quartz;
+
+import org.quartz.*;
+import org.quartz.impl.StdSchedulerFactory;
+
+import java.io.InputStream;
+import java.util.Properties;
+
+import static org.quartz.JobBuilder.*;
+import static org.quartz.TriggerBuilder.*;
+import static org.quartz.SimpleScheduleBuilder.*;
+
+public class AlertRabbit {
+
+    private int interval;
+
+    public static void main(String[] args) {
+
+        AlertRabbit rabbit = new AlertRabbit();
+        rabbit.init();
+
+        try {
+            Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
+            scheduler.start();
+            JobDetail job = newJob(Rabbit.class).build();
+            SimpleScheduleBuilder times = simpleSchedule()
+                    .withIntervalInSeconds(rabbit.interval)
+                    .repeatForever();
+            Trigger trigger = newTrigger()
+                    .startNow()
+                    .withSchedule(times)
+                    .build();
+            scheduler.scheduleJob(job, trigger);
+        } catch (SchedulerException se) {
+            se.printStackTrace();
+        }
+    }
+
+    public static class Rabbit implements Job {
+        @Override
+        public void execute(JobExecutionContext context) throws JobExecutionException {
+            System.out.println("Rabbit runs here ...");
+        }
+    }
+
+    public void init() {
+        try (InputStream in =
+                     AlertRabbit.class.getClassLoader().getResourceAsStream("rabbit.properties")) {
+            Properties config = new Properties();
+            config.load(in);
+            this.interval = Integer.parseInt(config.getProperty("rabbit.interval"));
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
+        }
+    }
+}
