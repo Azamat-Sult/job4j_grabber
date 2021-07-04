949e18626e06211f837ad9b554dfa9e31a10206f
Azamat-Sult <asultangareev@gmail.com>
2021-07-04 23:10:33 +0500

    2.1. Преобразование даты [#289476 #174116]. Исправление 1

--- src/main/java/ru/job4j/grabber/utils/SqlRuDateTimeParser.java
+++ src/main/java/ru/job4j/grabber/utils/SqlRuDateTimeParser.java
@@ -12,38 +12,31 @@ public class SqlRuDateTimeParser implements DateTimeParser {
         String[] split = parse.split(" ");
 
         if (split.length == 4) {
-            switch (split[1]) {
-                case "янв" -> parse = parse.replace("янв", "янв.");
-                case "фев" -> parse = parse.replace("фев", "февр.");
-                case "мар" -> parse = parse.replace("мар", "мар.");
-                case "апр" -> parse = parse.replace("апр", "апр.");
-                case "май" -> parse = parse.replace("май", "мая");
-                case "июн" -> parse = parse.replace("июн", "июн.");
-                case "июл" -> parse = parse.replace("июл", "июл.");
-                case "авг" -> parse = parse.replace("авг", "авг.");
-                case "сен" -> parse = parse.replace("сен", "сент.");
-                case "окт" -> parse = parse.replace("окт", "окт.");
-                case "ноя" -> parse = parse.replace("ноя", "нояб.");
-                case "дек" -> parse = parse.replace("дек", "дек.");
-            }
+            parse = switch (split[1]) {
+                case "янв" -> parse.replace("янв", "янв.");
+                case "фев" -> parse.replace("фев", "февр.");
+                case "мар" -> parse.replace("мар", "мар.");
+                case "апр" -> parse.replace("апр", "апр.");
+                case "май" -> parse.replace("май", "мая");
+                case "июн" -> parse.replace("июн", "июн.");
+                case "июл" -> parse.replace("июл", "июл.");
+                case "авг" -> parse.replace("авг", "авг.");
+                case "сен" -> parse.replace("сен", "сент.");
+                case "окт" -> parse.replace("окт", "окт.");
+                case "ноя" -> parse.replace("ноя", "нояб.");
+                case "дек" -> parse.replace("дек", "дек.");
+                default -> throw new IllegalStateException("Unexpected value: " + split[1]);
+            };
         }
 
         if (split.length == 2) {
-            switch (split[0]) {
-                case "сегодня," -> {
-                    String todayDate =
-                            LocalDate.now()
-                                    .format(DateTimeFormatter.ofPattern("d MMM yy, "));
-                    parse = todayDate + split[1];
-                }
-                case "вчера," -> {
-                    String yesterdayDate =
-                            LocalDate.now()
-                                    .minusDays(1)
-                                    .format(DateTimeFormatter.ofPattern("d MMM yy, "));
-                    parse = yesterdayDate + split[1];
-                }
-            }
+            parse = switch (split[0]) {
+                case "сегодня," -> LocalDate.now()
+                        .format(DateTimeFormatter.ofPattern("d MMM yy, ")) + split[1];
+                case "вчера," -> LocalDate.now()
+                        .minusDays(1).format(DateTimeFormatter.ofPattern("d MMM yy, ")) + split[1];
+                default -> throw new IllegalStateException("Unexpected value: " + split[0]);
+            };
         }
 
         DateTimeFormatter formatter =
