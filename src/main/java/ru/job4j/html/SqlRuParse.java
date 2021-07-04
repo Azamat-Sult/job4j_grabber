5317eb7cf0c69a9d0f77546924f7f066b8b0842b
Azamat-Sult <asultangareev@gmail.com>
2021-07-04 19:21:09 +0500

    2.1.1. Парсинг https://www.sql.ru/forum/job-offers/3 [#285210]

--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -6,16 +6,25 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 public class SqlRuParse {
+
+    private static int pagesToParse = 5;
+
     public static void main(String[] args) throws Exception {
-        Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers").get();
-        Elements row1 = doc.select(".postslisttopic");
-        Elements row2 = doc.select("td:nth-child(6)");
-        for (int index = 0; index < row1.size(); index++) {
-            Element href = row1.get(index).child(0);
-            System.out.println("-----------------------------------------------------");
-            System.out.println(href.attr("href"));
-            System.out.println(href.text());
-            System.out.println(row2.get(index).text());
+        for (int page = 1; page <= pagesToParse; page++) {
+            System.out.println("////////////////////////////////////////////////////////");
+            System.out.println("Parsing " + page + " page of " + pagesToParse + " pages");
+            System.out.println("////////////////////////////////////////////////////////");
+            System.out.println();
+            Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers/" + page).get();
+            Elements row1 = doc.select(".postslisttopic");
+            Elements row2 = doc.select("td:nth-child(6)");
+            for (int index = 0; index < row1.size(); index++) {
+                Element href = row1.get(index).child(0);
+                System.out.println(href.attr("href"));
+                System.out.println(href.text());
+                System.out.println(row2.get(index).text());
+                System.out.println("-----------------------------------------------------------------");
+            }
         }
     }
 }
