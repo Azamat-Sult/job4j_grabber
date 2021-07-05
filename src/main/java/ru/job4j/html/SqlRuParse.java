9801bdb6b2c9cb5cac15a5708a626bdbf61a4da7
Azamat-Sult <asultangareev@gmail.com>
2021-07-05 18:24:01 +0500

    2.3. Загрузка деталей поста. [#285212]

--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -5,12 +5,38 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import java.io.IOException;
+
 public class SqlRuParse {
 
-    private static int pagesToParse = 5;
+    //private static final int pagesToParse = 5;
+
+    public void parsePost(String url) throws IOException {
+        Document page = Jsoup.connect(url).get();
+
+        Elements postTextBlock
+                = page.select("table:nth-child(3) > tbody > tr:nth-child(2) > td:nth-child(2)");
+        Elements postDateBlock
+                = page.select("table:nth-child(3) > tbody > tr:nth-child(3) > td");
+
+        Element postText = postTextBlock.get(0);
+        String post = postText.text();
+        System.out.println(post);
+
+        Element postDate = postDateBlock.get(0);
+        String[] split = postDate.text().split(" ");
+        String dateOfPost = "";
+        if (split.length == 8) {
+            dateOfPost = split[0] + " " + split[1];
+        }
+        if (split.length == 10) {
+            dateOfPost = split[0] + " " + split[1] + " " + split[2] + " " + split[3];
+        }
+        System.out.println(dateOfPost);
+    }
 
     public static void main(String[] args) throws Exception {
-        for (int page = 1; page <= pagesToParse; page++) {
+        /*for (int page = 1; page <= pagesToParse; page++) {
             System.out.println("////////////////////////////////////////////////////////");
             System.out.println("Parsing " + page + " page of " + pagesToParse + " pages");
             System.out.println("////////////////////////////////////////////////////////");
@@ -25,6 +51,8 @@ public class SqlRuParse {
                 System.out.println(row2.get(index).text());
                 System.out.println("-----------------------------------------------------------------");
             }
-        }
+        }*/
+        SqlRuParse parser = new SqlRuParse();
+        parser.parsePost("https://www.sql.ru/forum/1325330/lidy-be-fe-senior-cistemnye-analitiki-qa-i-devops-moskva-do-200t");
     }
 }
