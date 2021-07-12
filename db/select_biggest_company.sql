d4ed71b57ff0bc14462d9193b42e8596ddffad43
Azamat-Sult <asultangareev@gmail.com>
2021-07-12 23:42:22 +0500

    2. SQL Select by company [#2760]. Исправление 1

--- db/select_biggest_company.sql
+++ db/select_biggest_company.sql
@@ -1,11 +1,9 @@
-select counted.name, counted.count from
-(select c.name, count(p.company_id)
+create view counted as
+select c.name, count(p.company_id)
  from company as c
  join person as p
  on c.id = p.company_id
- group by c.name) as counted
- where counted.count = (select max(count) FROM (select c.name, count(p.company_id)
- from company as c
- join person as p
- on c.id = p.company_id
- group by c.name) as counted);
+ group by c.name;
+
+select counted.name, counted.count from counted
+ where counted.count = (select max(count) from counted);
